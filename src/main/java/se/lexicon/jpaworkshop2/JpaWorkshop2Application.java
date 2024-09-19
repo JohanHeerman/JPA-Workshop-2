d20238b795982d878453d1b74d40875f839526a6
Johan Heerman <johanheerman@gmail.com>
2024-09-19 09:58:29 +0200

    Initial commit

---
+++ src/main/java/se/lexicon/jpaworkshop2/JpaWorkshop2Application.java
@@ -0,0 +1,13 @@
+package se.lexicon.jpaworkshop2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaWorkshop2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaWorkshop2Application.class, args);
+	}
+
+}
---
+++ src/test/java/se/lexicon/jpaworkshop2/JpaWorkshop2ApplicationTests.java
@@ -0,0 +1,13 @@
+package se.lexicon.jpaworkshop2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaWorkshop2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
